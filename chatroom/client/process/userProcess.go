03eea9c0c9c1b879d292209dd072d40832d50965
root <2575003872@qq.com>
2022-01-04 09:49:58 +0800

    first commit

---
+++ chatroom/client/process/userProcess.go
@@ -0,0 +1,93 @@
+package process
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"net"
+	"work-place/src/chatroom/client/util"
+	"work-place/src/chatroom/common"
+)
+
+type UserProcess struct {
+}
+
+func (this *UserProcess) Login(userId int, userPwd string) {
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8889")
+
+	if err != nil {
+
+		fmt.Println(err)
+	}
+	//延时关闭
+	defer conn.Close()
+	//初始化message
+	var message common.Message
+	message.Type = common.LoginMessageType
+
+	var data common.LoginMessage
+	data.UserId = userId
+	data.UserPwd = userPwd
+
+	bytes, _ := json.Marshal(data)
+	message.Data = string(bytes)
+
+	//序列化message
+	mesgJson, err := json.Marshal(message)
+	if err != nil {
+
+		fmt.Println(err)
+	}
+
+	//传输的过程中需要的是一个byte[]切片，所以需要将数据长度转换为一个uint类型的变量
+	var pkgLen uint32
+	pkgLen = uint32(len(mesgJson))
+
+	var length [4]byte
+	binary.BigEndian.PutUint32(length[0:4], pkgLen)
+
+	n, err := conn.Write(length[:4])
+
+	if n != 4 || err != nil {
+
+		fmt.Println("传输异常")
+		return
+	}
+
+	_, err = conn.Write(mesgJson)
+	if err != nil {
+
+		fmt.Println(err)
+		return
+	}
+
+	//处理服务端给客户端的返回信息
+	transFer := &util.TransFer{Conn: conn}
+	resMes, err := transFer.HandleRequest()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//将其Data值反序列化为一个responseMes
+	var responseMes common.ResponseMessage
+	err = json.Unmarshal([]byte(resMes.Data), &responseMes)
+	if err != nil {
+		return
+	}
+
+	if responseMes.Code == 200 {
+		fmt.Println("登陆成功")
+
+		//登陆成功，显示菜单
+		for {
+			ShowMenu()
+		}
+	} else if responseMes.Code == 500 {
+
+		fmt.Println(responseMes.Error)
+		return
+	}
+}
---
+++ chatroom/common/message.go
@@ -0,0 +1,29 @@
+package common
+
+const (
+	LoginMessageType = "LoginMes"
+	LoginResType     = "LoginResMes"
+	RegisterType     = "RegisterMes"
+)
+
+type Message struct {
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
+
+type LoginMessage struct {
+	UserId   int    `json:"user_id"`
+	UserName string `json:"user_name"`
+	UserPwd  string `json:"user_pwd"`
+}
+
+type ResponseMessage struct {
+	Code  int    `json:"code"`
+	Error string `json:"error"`
+}
+
+type RegisterMessage struct {
+	UserId   int    `json:"user_id"`
+	UserName string `json:"user_name"`
+	UserPwd  string `json:"user_pwd"`
+}
